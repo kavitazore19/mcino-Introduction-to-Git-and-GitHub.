49a351b4125ed276a1d5931496ded4c4a460df18
kavitazore19 <zorekavita100@gmail.com>
2026-01-28 19:34:17 +0530

    Add AboutUs component for team introduction

---
+++ AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>We are a team dedicated to front-end development using React.</p>
+    </div>
+  );
+};
+
+export default AboutUs;
