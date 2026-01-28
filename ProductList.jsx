c8fbc6e75136458c7fc9b86e6d2a0b85af3895f1
kavitazore19 <zorekavita100@gmail.com>
2026-01-28 19:51:58 +0530

    Add ProductList component to display products

---
+++ ProductList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ProductList = ({ products }) => {
+  return (
+    <div className="product-list">
+      {products.map(product => (
+        <div key={product.id} className="product-card">
+          <img src={product.image} alt={product.name} />
+          <h3>{product.name}</h3>
+          <p>${product.price}</p>
+          <button>Add to Cart</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
